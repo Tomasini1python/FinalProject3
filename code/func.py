7141b9f86d1b70258aa862273100612cfc1bc5b9
Tomás Lopes <100950936+Tomasini1python@users.noreply.github.com>
2024-05-25 13:45:50 +0100

    Making the functions

---
+++ code/func.py
@@ -0,0 +1,12 @@
+from g4f import client
+
+def diferent_func(text:str):
+    if "play" in text:
+        text.index('play')+4
+
+    else:
+        cliente = client.Client()
+        responsez
+
+
+
--- code/main.py
+++ code/main.py
@@ -1,4 +1,10 @@
+from voiceRec import *
+from func import *
 
+while True:
+    text = voice_recognition()
+    print(text)
+    diferent_func(text)
 
 
 
